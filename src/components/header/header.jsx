0038b460b2c280c898a3bfdd14c3f24de61c33ab
mayelb <mayelb22@gmail.com>
2024-05-12 16:14:03 -0300

    header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,14 +3,17 @@ import "./header.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Badge from 'react-bootstrap/Badge';
  
 export const Header = () => {
   return (
     <>
       <Navbar expand="lg" className="nav">
-      <img src="" alt="Logo" className="brand">  
-         
-        </img>
+        <div  className="brand">
+        <h2> 
+        Small
+        </h2>
+        </div>
         <Container>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
